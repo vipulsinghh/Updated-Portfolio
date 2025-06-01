91854b1b96033a328814e79167b5793fe38c4058
Vipul Singh <vipulsingh0110@gmail.com>
2025-06-01 10:50:18 +0000

    make chat section work

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -3,5 +3,6 @@ import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
   plugins: [googleAI()],
-  model: 'googleai/gemini-2.0-flash',
+  model: 'googleai/gemini-1.5-flash-latest',
 });
+
